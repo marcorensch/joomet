d44f8399530d8145a51b9fc8cac501531709e5c4
Marco Rensch <marco.rensch@nx-designs.ch>
2022-05-01 17:18:55 +0200

    Added Detail view for project

--- src/views/projects/ProjectDetail.vue
+++ src/views/projects/ProjectDetail.vue
@@ -9,8 +9,7 @@
 
         <div class="uk-flex uk-flex-right">
           <div class="uk-margin-large-right uk-button-group" id="listtypeBtnGroup">
-            <button class="uk-button nx-button-default uk-button-small uk-position-relative" :class="{'nx-selected':listLayout=='grid'}" @click="setListType('grid')"><font-awesome-icon icon="grip" /></button>
-            <button class="uk-button nx-button-default uk-button-small uk-position-relative" :class="{'nx-selected':listLayout=='list'}" @click="setListType('list')"><font-awesome-icon icon="bars" /></button>
+
           </div>
           <div>
             <router-link :to="{name: 'Projects.New'}" title="New Project" class="uk-button nx-button-default nx-button-success uk-button-small"><font-awesome-icon icon="folder-plus" /></router-link>
